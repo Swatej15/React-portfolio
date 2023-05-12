657b8305206c1effc4eaf3bece20e3fc99dc0b79
Swatej15 <swatejparmar0315@gmail.com>
2023-05-12 16:33:57 +0530

    resume

--- src/components/SocialLinks.jsx
+++ src/components/SocialLinks.jsx
@@ -42,7 +42,7 @@ const SocialLinks = () => {
           Resume <BsFillPersonLinesFill size={30} />
           </>
         ),
-        href: '/Resume.pdf',
+        href: '/Resume3.pdf',
         style: 'rounded-br-md',
         download: true,
       },
