69932f2019297947ab83b029ec574aa92f09c458
Swatej15 <swatejparmar0315@gmail.com>
2023-06-19 20:19:58 +0530

    chnage

--- src/components/Portfolio.jsx
+++ src/components/Portfolio.jsx
@@ -51,7 +51,7 @@ const Portfolio = () => {
       <div className="max-w-screen-lg p-4 mx-auto flex flex-col justify-center w-full h-full">
         <div className="pb-8">
           <p className="text-4xl font-bond inline border-b-4 border-gray-500">Portfolio</p>
-          <p className="py-6">I am working on some of project that are not done yet but here my one of project is traveling Check out </p>
+          <p className="py-6">I am working on some of project that are not done yet but here my two of project is traveling and NETFLIX clone Check out </p>
         </div>
 
         <div className="grid sm:grid-cols-2 md:grid-cols-3 gap-8 px-12 sm:px-0">
